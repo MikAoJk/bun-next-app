359d80b67a52a995a69546c19e0f0ec58fd31881
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2023-09-25 08:48:46 +0200

    Try to deploy app to Github pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+    images: {
+        loader: 'akamai',
+        path: '/'
+    }
+}
 
 module.exports = nextConfig
