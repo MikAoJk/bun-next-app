906113054beec73a7c626b1d33ff5cc4663ed907
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2023-12-31 14:31:24 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     basePath: process.env.NEXT_PUBLIC_BASE_PATH,
     images: {
         loader: 'akamai',
-        path: '/'
+        path: '/public'
     },
     output: 'export'
 }
